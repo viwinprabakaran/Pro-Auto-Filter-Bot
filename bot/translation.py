57df5f82487d7ab7edbc4f0950e1ae75a2875c42
viwinprabakaran <84952869+viwinprabakaran@users.noreply.github.com>
2021-07-12 21:20:05 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,48 +10,21 @@ class Translation(object):
 
 Request Your Movies in <b> "https://t.me/New_CinemaZ_Group” </b>
 
-Don't Add Me in any Groups You Have I won't Work 😠
+Don't Add Me in any Groups You Have, I won't Work 😠
 </i>"""    
     
     HELP_TEXT = """
-
-<i>
--> Don't Add Me To Any Group And Make Me Admin
--> Don't Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
 <b><i> https://t.me/New_CinemaZ_Group </i></b>
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <b> IᖇOᑎ ᗰᗩᑎ Bot</b>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/DarkShAdOwZzZ">IᖇOᑎ ᗰᗩᑎ</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/DarkShAdOwZzZ"></a></i></b>
 
-<b>➥ Language</b> : <code>Python 3</code>
+<b>➥ Language</b> : <code>Python</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ Library</b> : <i><a href="https://t.me/joinchat/yJcv2OAZ3JQwYjNl">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Support Us</b> : <i><a href="https://t.me/NC_linkz">NC_linkz</a></i>
+<b>➥ Support Us</b> : <i><a href="♻️ 𝙅𝙊𝙄𝙉 :- https://t.me/joinchat/yJcv2OAZ3JQwYjNl"></a></i>
 """
