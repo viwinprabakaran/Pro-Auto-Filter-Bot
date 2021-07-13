1a5db3c563992fcba5479cd2374eca44010be1c1
viwinprabakaran <84952869+viwinprabakaran@users.noreply.github.com>
2021-07-13 17:56:04 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -66,7 +66,7 @@ async def auto_filter(bot, update):
             file_link = filter.get("file_link")
             file_size = int(filter.get("file_size", "0"))
             
-            # from B to MiB
+            # from B to MB
             
             if file_size < 1024:
                 file_size = f"[{file_size} B]"
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}«🎬»{file_name}"
+            button_text = f"{file_size}«📁»{file_name}"
             
 
             if file_type == "video":
@@ -153,7 +153,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"🌟 Page 1/{len_result if len_result < max_pages else max_pages} 🌟", callback_data="ignore")
         ])
         
         
