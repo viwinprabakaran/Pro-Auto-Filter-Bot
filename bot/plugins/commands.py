30cc8ef7834291bc06abbeccb37f669fe95662c7
viwinprabakaran <84952869+viwinprabakaran@users.noreply.github.com>
2021-07-31 09:17:47 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"{file_name} \n @New_CinemaZ_Group,
+                caption = caption,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
